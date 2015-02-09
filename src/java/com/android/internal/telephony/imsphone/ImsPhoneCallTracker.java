cf9a426b3be39dc0729a08a81a1101e7ad7743c3
Suresh Koleti <skolet@codeaurora.org>
2015-02-09 14:33:47 +0530

    IMS: Handle FDN call failures
    Handle FDN call failure.
    
    Change-Id: I4c36584acf81053fabd44082a7d742812cad05fa
    CRs-Fixed: 792389

--- src/java/com/android/internal/telephony/imsphone/ImsPhoneCallTracker.java
+++ src/java/com/android/internal/telephony/imsphone/ImsPhoneCallTracker.java
@@ -1082,6 +1082,8 @@ public final class ImsPhoneCallTracker extends CallTracker {
             case ImsReasonInfo.CODE_LOCAL_POWER_OFF:
                 return DisconnectCause.POWER_OFF;
 
+            case ImsReasonInfo.CODE_FDN_BLOCKED:
+                return DisconnectCause.FDN_BLOCKED;
             default:
         }
 
